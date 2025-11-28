20642f324e7c72034764934ec4ba6a49e7e1027e
Marco Regragui Martins <marco781227@gmail.com>
2025-11-28 17:10:02 +0100

    format : added formatting standard

--- src/multicore/src/spinlock.c
+++ src/multicore/src/spinlock.c
@@ -2,12 +2,12 @@
 
 char mutex = 1;
 
-void lock(){
-  lock_mutex(&mutex, 0);
-  return;
+void lock() {
+    lock_mutex(&mutex, 0);
+    return;
 }
 
-void unlock(){
-  free_mutex(&mutex, 1);
-  return;
+void unlock() {
+    free_mutex(&mutex, 1);
+    return;
 }
